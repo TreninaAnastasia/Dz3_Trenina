baaccd5de53dbaaf37db235322589ac1a169e339
Nastya <treninanst@gmail.com>
2023-06-18 21:45:09 +0400

    Added 3 zadanie

---
+++ 3Zadanie/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number; 
+}
+int N = Prompt ("Введите любое число : ");
+Find(N);
+void Find( int n)
+{
+    int i = 1;
+    while ( i <= N)
+    {
+        Console.Write( i * i * i + " ");
+        i ++;
+    }
+}
